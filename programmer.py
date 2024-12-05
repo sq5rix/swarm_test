298ff19aaf872efafc611b0866808f44bcf6e1e4
tom wawer <sq5rix@gmail.com>
2024-12-05 13:35:35 +0100

    tester

--- programmer.py
+++ programmer.py
@@ -14,22 +14,31 @@ def order_code():
     return agent_b
 
 
+def test_code():
+    return agent_c
+
+
 agent_a = Agent(
     name="ProjectManager",
     model=model_list[QWEN7],
     instructions="You are project manager.",
-    functions=[order_code],
+    functions=[order_code, test_code],
 )
 
 agent_b = Agent(
     name="Programmer",
-    model=model_list[QWEN7a],
+    model=model_list[QWEN7],
     instructions=PROGRAM_PYTHON_PROMPT,
 )
 
+agent_c = Agent(
+    name="Tester",
+    model=model_list[QWEN7],
+    instructions=TESTER_PROMPT,
+)
+
 response = client.run(
-    agent=agent_a,
-    messages=[{"role": "user", "content": PROJECT_MANAGER_PROMPT}],
+    agent=agent_a, messages=[{"role": "user", "content": PROJECT_MANAGER_PROMPT}]
 )
 
 print(response.messages[-1]["content"])
